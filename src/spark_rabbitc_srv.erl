b6ead7ff44b8e9628ba68c2f1898ae68662418dc
root <root@chat.dev0.spark.net>
2013-06-24 20:24:01 -0400

    fix up supervisor

---
+++ src/spark_rabbitc_srv.erl
@@ -0,0 +1,8 @@
+-module(spark_rabbitc_srv).
+-behaviour(gen_server).
+
+-export([]).
+
+-export([start_link/0]).
+
+-export([]).
--- src/spark_rabbitc_sup.erl
+++ src/spark_rabbitc_sup.erl
@@ -13,13 +13,14 @@
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 -define(DEFAULT_RESTART,{one_for_one, 5, 10}).
 -define(SPAWN_OPTS, {fullsweep_after, 60}).
+-define(SERVER, ?MODULE).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, [{spawn_opts, ?SPAWN_OPTS}]).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [{spawn_opts, ?SPAWN_OPTS}]).
 
 %% ===================================================================
 %% Supervisor callbacks
