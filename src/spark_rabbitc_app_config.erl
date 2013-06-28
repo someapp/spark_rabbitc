15a4034cc97c9f1a721b9716aef784dd51f1d35f
root <root@chat.dev0.spark.net>
2013-06-27 20:04:49 -0400

    fixing compile error

--- src/spark_rabbitc_app_config.erl
+++ src/spark_rabbitc_app_config.erl
@@ -3,37 +3,35 @@
 
 -export([
    load_config/0,
-   load_config/1,
+	 load_config/1,
    load_config/2,
    load_rest_config/1,
    load_amqp_config/1,
-   get_log_config/0,
+	 get_log_config/0,
    get_full_resource_url/2,
    get_rest_environment/1,
-   get_rabbit_environment/1,
+	 get_rabbit_environment/1,
    get_api_vsn/1,
 
    spark_api_endpoint/1, 
-   spark_app_id/1,
+	 spark_app_id/1,
    spark_brand_id/1,
-   spark_client_secret/1,
-   spark_oauth_access_token/1, 
-   spark_communityid_brandid_map/1,
-   auth_profile_miniProfile/1, 
-   profile_memberstatus/1, 
-   send_missed_im/1,
-   rabbitmq_endpoint/1,
-   rest_client_timeout_in_sec/1,
-   rest_call_retry_attempt/1,
-   rabbitmq_client_timeout_in_sec/1,
-   rabbitmq_client_retry_attempt/1,
+	 spark_client_secret/1,
+	 spark_oauth_access_token/1, 
+	 spark_communityid_brandid_map/1,
+	 auth_profile_miniProfile/1, 
+	 profile_memberstatus/1, 
+	 send_missed_im/1,
+
+ 	 rest_client_timeout_in_sec/1,
+ 	 rest_call_retry_attempt/1,
 
    get_rabbit_username/1,
    get_rabbit_password/1,
-   get_rabbit_virtual_host/1,
+   get_rabbit_vhost/1,
    get_rabbit_exchange/1,
    get_rabbit_client_properties/1,
-   get_rabbit_conneciton_timeout/1,
+   get_rabbit_connection_timeout/1,
    get_rabbit_ticket/1,
    get_rabbit_type/1,
    get_rabbit_passive/1,
@@ -61,7 +59,7 @@
 %-define(APP_ENV,ejabberd_auth_spark).
 
 
--deinfe(SERVER,?MODULE).
+-define(SERVER,?MODULE).
 -define(SERVER_CONF, spark_config).
 
 %% ===================================================================
@@ -97,18 +95,20 @@ load_config(FileName, Key) ->
             {error, Reason}
     end.
 
-get_full_resource_url(BaseUrl, ResourceUrl) ->
+get_full_resource_url(CfgList, Key)->
+   BaseUrl = spark_api_endpoint(CfgList),
+   ResUrl = get_config_value_env(Key,CfgList),
    ApiVsn = get_api_vsn(CfgList),
-   FulllUrl = lists:concat([BaseUrl, ResourceUrl]),
+   FullUrl = lists:concat([BaseUrl, ResUrl]),
    ReplaceOpts = [global, {return, list}],
-   Ret = re:replace(FullUrl,"ApiVsn", ApiVsn, ReplaceOpts)
-   list_to_binary(Ret).
+   Ret = re:replace(FullUrl,"ApiVsn", ApiVsn, ReplaceOpts),
+   to_bitstring(Ret).
 
 get_https_or_http()->
   http.
 
 get_ssl()->
-  false;
+  false.
 
 get_rabbit_environment(CfgList) ->
    get_config_value_env(rabbitc_env, CfgList, required). 
@@ -123,47 +123,41 @@ get_api_vsn(CfgList)->
 %% @end
 -spec spark_api_endpoint(list())-> url() | fatalError().
 spark_api_endpoint(CfgList) ->
-   get_config_value_env(spark_api_endpoint,CfgList, required).
+   get_config_value_env(spark_api_endpoint,CfgList, default_api_endpoint()).
 
 -spec spark_app_id(list()) -> integer() | fatalError().
 spark_app_id(CfgList)->
    get_config_value_env(spark_app_id,CfgList, required).   
 
--spec spark_app_id(list()) -> integer() | undef.
+-spec spark_brand_id(list()) -> integer() | undef.
 spark_brand_id(CfgList)->
    get_config_value_env(spark_brand_id,CfgList, undef). 
 
 -spec spark_client_secret(list()) -> string() | fatalError().
 spark_client_secret(CfgList)->
-   get_config_value_env(spark_client_secret,CfgList, required).
+   get_config_value_env(spark_client_secret,CfgList, default_client_secret()).
 
 %% @doc Get the oauth access token environment variable.
 -spec spark_oauth_access_token(list()) -> accessToken() | undefined.
 spark_oauth_access_token(CfgList) ->
-   get_full_resource_url(CfgList, spark_create_oauth_accesstoken).
+
+   get_full_resource_url(CfgList,default_create_oauth_accesstoken()).
 
 spark_communityid_brandid_map(CfgList)->
-   get_config_value_env(community2brandId, CfgList,required).
+   get_config_value_env(community2brandId, CfgList,default_community2brandId()).
 
 %% @doc Get api miniprofile environment variable.
 %% @end
 -spec auth_profile_miniProfile(list())-> string() | undefined.
 auth_profile_miniProfile(CfgList) ->
-   get_full_resource_url(CfgList,auth_profile_miniProfile).
+   get_full_resource_url(CfgList,default_auth_profile_miniProfile()).
 
 profile_memberstatus(CfgList)->
-   get_full_resource_url(CfgList, profile_memberstatus).
-
+   get_full_resource_url(CfgList, default_profile_memberstatus()).
 
 send_missed_im(CfgList)->
-   get_full_resource_url(CfgList,send_missed_im).
-
-get_full_resource_url(CfgList, Key)->
-   BaseUrl = spark_api_endpoint(CfgList),
-   ResUrl = get_config_value_env(Key,CfgList),
-   get_full_resource_url(BaseUrl, ResUrl).
-     
-
+   get_full_resource_url(CfgList,default_send_missed_im()).
+  
 %% @doc Get the rabbitmq endpoint environment variable.
 %% @end
 -spec rest_client_timeout_in_sec(list()) -> integer().
@@ -178,100 +172,109 @@ rest_call_retry_attempt(CfgList) ->
 
 %% ======================RabbitMq ======================================
 get_rabbit_username(CfgList) ->
-   get_config_value_env(username,ConfList,<<"guest">>).
+   get_config_value_env(username,CfgList,<<"guest">>).
 
 get_rabbit_password(CfgList) ->
-   get_config_value_env(password,ConfList,<<"guest">>).
+   get_config_value_env(password,CfgList,<<"guest">>).
 
 get_rabbit_vhost(CfgList) ->
-   get_config_value_env(virtual_host,ConfList,<<"/">>).
+   get_config_value_env(virtual_host,CfgList,<<"/">>).
 
 get_rabbit_exchange(CfgList) ->
-   get_config_value_env(exchange,ConfList,<<"">>).
+   get_config_value_env(exchange,CfgList,<<"">>).
 
 get_rabbit_client_properties(CfgList) ->
-   get_config_value_env(client_properties,ConfList,[]).
+   get_config_value_env(client_properties,CfgList,[]).
 
-get_rabbit_conneciton_timeout(CfgList) ->
-   get_config_value_env(connection_timeout,ConfList,infinity).
+get_rabbit_connection_timeout(CfgList) ->
+   get_config_value_env(connection_timeout,CfgList,infinity).
 
 get_rabbit_ticket(CfgList) ->
-   get_config_value_env(ticket,ConfList,0).
+   get_config_value_env(ticket,CfgList,0).
 
 get_rabbit_type(CfgList) ->
-   get_config_value_env(type,ConfList,<<"direct">>).
+   get_config_value_env(type,CfgList,<<"direct">>).
 
 get_rabbit_passive(CfgList) ->
-   get_config_value_env(passive,ConfList,false).
+   get_config_value_env(passive,CfgList,false).
 
 get_rabbit_durable(CfgList) ->
-   get_config_value_env(durable,ConfList,false).
+   get_config_value_env(durable,CfgList,false).
 
 get_rabbit_auto_delete(CfgList) ->
-   get_config_value_env(auto_delete,ConfList,false).
+   get_config_value_env(auto_delete,CfgList,false).
 
 get_rabbit_internal(CfgList) ->
-   get_config_value_env(internal,ConfList,false).
+   get_config_value_env(internal,CfgList,false).
 
 get_rabbit_nowait(CfgList) ->
-   get_config_value_env(nowait,ConfList,false).
+   get_config_value_env(nowait,CfgList,false).
 
 get_rabbit_arguments(CfgList) ->
-   get_config_value_env(arguments,ConfList,[]).
+   get_config_value_env(arguments,CfgList,[]).
 
 get_rabbit_channel_count(CfgList) ->
-   get_config_value_env(channel_count,ConfList,[]).
+   get_config_value_env(channel_count,CfgList,[]).
+
+get_rabbit_host(CfgList)->
+   get_config_value_env(host,CfgList,"localhost").
+
+get_rabbit_port(CfgList)->
+   get_config_value_env(port,CfgList,5672).
+
+get_rabbit_heartbeat(CfgList) ->
+   get_config_value_env(heartbeat,CfgList,5).
+
 
 %% ======================================================================
 
 
 %% @private
 get_config_value_env(Key,CfgList) ->
-   R = proplists:get_value(Key,ConfList,{warn, undef}),
+   R = proplists:get_value(Key,CfgList,{warn, undef}),
    to_bitstring(R).
 
 get_config_value_env(Key, CfgList,required)->
-   case proplists:get_value(Key,ConfList,{error, undef}) of
+   case proplists:get_value(Key,CfgList,{error, undef}) of
         {error, undef} -> erlang:exit(undef);
         V -> to_bitstring(V)
-   end.
+   end;
 
 get_config_value_env(Key, CfgList,Default) ->
-   R = proplists:get_value(Key,ConfList,Default),
+   R = proplists:get_value(Key,CfgList,Default),
    to_bitstring(R).
 
-to_bistring(S) when is_list(S)->
-  erlang:list_to_bitstring(S).
+to_bitstring(S) when is_list(S)->
+   erlang:list_to_bitstring(S);
 
 to_bitstring(_)->
   <<"">>.
 
-load_setting(ApiConf,RabbitConf),
-   AmqpParam = load_amqp_config(RabbitConf),
-   RestParam = load_rest_config(ApiConf).
-   {ok, #state{
- 	      amqp_param = AmqpParm,
-	      rest_param = RestParm
-   }}.
-
+get_format(_CfgList)->
+  text.
 load_amqp_config(RabbitConf) ->
-   ConfList = load_config(RabbitConf, spark_rabbit),
-   Environment = get_rabbit_environment(ConfList),
-   UserName    = get_rabbit_username(ConfList),
-   Password    = get_rabbit_password(ConfList),
-   VirtualHost = get_rabbit_virtual_host(ConfList),
-   Exchange     = get_rabbit_exchange(ConfList),
-   Client_properties = get_rabbit_client_properties(ConfList),
-   Connection_timeout = get_rabbit_conneciton_timeout(ConfList),
-   Ticket       = get_rabbit_ticket(ConfList),
-   Type         = get_rabbit_type(ConfList),
-   Passive      = get_rabbit_passive(ConfList),
-   Durable      = get_rabbit_durable(ConfList),
-   AutoDelete   = get_rabbit_auto_delete(ConfList),
-   Internal     = get_rabbit_internal(ConfList),
-   Arguments    = get_rabbit_arguments(ConfList),
-   NoWait       = get_rabbit_nowait(ConfList),
-   ChannelCount = get_rabbit_channel_count(ConfList,[]),
+   CfgList = load_config(RabbitConf, spark_rabbit),
+
+   Environment	= get_rabbit_environment(CfgList),
+   Format = get_format(CfgList),
+   UserName    = get_rabbit_username(CfgList),
+   Password    = get_rabbit_password(CfgList),
+   Host = get_rabbit_host(CfgList),
+   Port = get_rabbit_port(CfgList),
+   HeartBeat = get_rabbit_heartbeat(CfgList),
+   VirtualHost = get_rabbit_vhost(CfgList),
+   Exchange     = get_rabbit_exchange(CfgList),
+   Client_properties = get_rabbit_client_properties(CfgList),
+   Connection_timeout = get_rabbit_connection_timeout(CfgList),
+   Ticket       = get_rabbit_ticket(CfgList),
+   Type         = get_rabbit_type(CfgList),
+   Passive      = get_rabbit_passive(CfgList),
+   Durable      = get_rabbit_durable(CfgList),
+   AutoDelete   = get_rabbit_auto_delete(CfgList),
+   Internal     = get_rabbit_internal(CfgList),
+   Arguments    = get_rabbit_arguments(CfgList),
+   NoWait       = get_rabbit_nowait(CfgList),
+   ChannelCount = get_rabbit_channel_count(CfgList),
    AmqpParam   = #amqp_params_network{
                   	username     = ensure_binary(UserName),
                         password     = Password,
@@ -280,16 +283,18 @@ load_amqp_config(RabbitConf) ->
                         port         = Port
                  },
    #rabbit_conf{
-
-
+      environment = Environment,
+      format  = Format,
+      
+      amqp_params = AmqpParam
    }.
 
 load_rest_config(ApiConf)->
-   ConfList = load_config(ApiConf, spark_rest),
+   CfgList = load_config(ApiConf, spark_rest),
    Environment	= get_rest_environment(CfgList),
    Api_endpoint = spark_api_endpoint(CfgList),
    App_id = spark_app_id(CfgList),
-   Brand_id = spark_brand_id(ConfList),
+   Brand_id = spark_brand_id(CfgList),
    Client_secret = spark_client_secret(CfgList),
    Create_oauth_accesstoken = spark_oauth_access_token(CfgList),
    Auth_profile_miniProfile= auth_profile_miniProfile(CfgList),
@@ -297,17 +302,18 @@ load_rest_config(ApiConf)->
    IdMap = spark_communityid_brandid_map(CfgList),
    ResUrls0 =  dict:new(),
    ResUrl1 = dict:append(oauth_access_token, Create_oauth_accesstoken, ResUrls0),
-   ResUrl2 = dict:append(auth_profile_miniProfile, Auth_profile_miniProfile, ResUrls1),
+   ResUrl2 = dict:append(auth_profile_miniProfile, Auth_profile_miniProfile, ResUrl1),
    ResUrl3 = dict:append(profile_memberstatus, Member_Status, ResUrl2),
 
    #rest_conf{
 	     base_url = Api_endpoint,
+       	     rest_env = Environment,
 	     resource_urls = ResUrl3,
 	     app_id = App_id,
 	     client_secret = Client_secret,
-       idMap = IdMap,
-       retry = rest_call_retry_attempt(),
-       timeout = rest_client_timeout_in_sec(),
+             idMap = IdMap,
+             retry = rest_call_retry_attempt(),
+             timeout = rest_client_timeout_in_sec(),
 	     http_method = get_https_or_http(),
 	     ssl_key = get_ssl()
    }.
@@ -328,8 +334,11 @@ default_api_endpoint()->
 default_client_secret()->
   "nZGVVfj8dfaBPKsx_dmcRXQml8o5N-iivf5lBkrAmLQ1". 
 
-default_spark_create_oauth_accesstoken()->
+default_create_oauth_accesstoken()->
   "/brandId/{brandId}/oauth2/accesstoken/application/{applicationId}".
+
+default_send_missed_im()->
+  "/brandId/{brandId}/instantmessenger/missedIM".
 
 default_auth_profile_miniProfile()->
   "/brandId/{brandId}/profile/attributeset/miniProfile/{targetMemberId}".
